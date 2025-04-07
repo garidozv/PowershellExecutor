535703d1aa997f4b8fa89127119b83c8d5986e22
garidos <113151588+garidos@users.noreply.github.com>
2025-04-07 11:16:14 +0200

    Add README examples

---
+++ Examples/example2_script.ps1
@@ -0,0 +1,20 @@
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
+Write-Error -Message "Error Stream: An error occurred!"
+
+"Output Stream: Processing..."
+
+$name = Read-Host -Prompt 'Your name:'
+
+Write-Warning -Message "Warning Stream: Something might be wrong..."
+
+Write-Information -MessageData "Information Stream: Just some info" -InformationAction 'Continue'
+
+Write-Error -Message "Error Stream: Another error occurred!"
+
+Write-Host 'Your name is' $name  -Separator ' --> ' -BackgroundColor Green  -ForegroundColor White
+
+Write-Verbose -Message "Verbose Stream: Some verbose details..." -Verbose
+
+$DebugPreference = 'Continue'
+Write-Debug -Message "Debug Stream: Debug information"
